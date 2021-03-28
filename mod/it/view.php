d659d22462ba38b1d4e841b3762d73e93d85aff3
nicolas-dalpe <nicolas.dalpe@knowledgeone.ca>
2021-03-28 18:03:42 -0400

    mod_it : Tmp commit

--- mod/it/view.php
+++ mod/it/view.php
@@ -70,6 +70,7 @@ $PAGE->set_url('/mod/it/view.php', array('id' => $cm->id));
 $PAGE->set_title('Test Interactive Transcript');
 $PAGE->set_heading(format_string($course->fullname));
 $PAGE->set_context($modulecontext);
+$PAGE->requires->js_call_amd('mod_it/it', 'init');
 
 echo $OUTPUT->header();
 echo '<div class="clearer"></div>';
@@ -90,11 +91,128 @@ if (trim(strip_tags($content['intro'], '<img>'))) {
 
 $view->outputview();
 
-echo '<p>';
-echo '<span id="marker_1" data-min="1" data-max="4">- Life ain\'t so great<br>- Yeah. there\'ll be dots in the mud</span>';
-echo '<span id="marker_2" data-min="4" data-max="8">- And life ain\'t Hollywood<br>- for any one of us</span>';
-echo '<span id="marker_3" data-min="8" data-max="12">- If ever you\'re in doubt.<br>- just get your wings out</span>';
-echo '<span id="marker_4" data-min="12" data-max="16">- It\'s alright. darling.<br>- get your wriggle on</span>';
+echo "\n\n\n --->\n";
+
+// Make a PHP object with the json settings from H5P.
+$VideoContent = json_decode($content['filtered']);
+
+// Get the transcript file path.
+$transcriptFile = $VideoContent->interactiveVideo->video->textTracks->videoTrack[1]->track->path;
+
+// Get the transcript file records.
+$file = $DB->get_record('files', array('filearea' => 'content', 'filename' => substr($transcriptFile, 6)), 'contenthash');
+$contenthash = $file->contenthash;
+
+// Get the transcript file path in  Moodle data folder.
+$transcriptPath = sprintf(
+    '/var/www/md_ec_moodle310/filedir/%s/%s/%s',
+    substr($contenthash, 0, 2),
+    substr($contenthash, 2, 2),
+    $contenthash
+);
+
+if (file_exists($transcriptPath)) {
+    $lines = file($transcriptPath);
+    $c = array();
+    $i = 0;
+
+    foreach ($lines as $key => $line) {
+
+        // Remove line break from empty lines.
+        $line = trim($line);
+
+        // Skip the empty lines and the WEBVTT file title.
+        if ($line == 'WEBVTT' || empty($line)) {
+            continue;
+        }
+
+        // Start the subtitle index.
+        if (intval(trim($line)) !== 0) {
+            $i = $line * 10;
+            $c[$i]['index'] = $i;
+            continue;
+        }
+
+        // Generate the timecode.
+        if (strstr($line, '-->') !== false) {
+            $timeline = explode('-->', $line);
+
+            $startdate = new DateTime($timeline[0]);
+            $c[$i]['startSecond'] = intval(date('s', $startdate->getTimestamp()));
+
+            $enddate = new DateTime($timeline[1]);
+            $c[$i]['endSecond'] = intval(date('s', $enddate->getTimestamp()));
+            continue;
+        } else {
+            // Get the subtitle content.
+            $c[$i]['content'] = format_text($line, FORMAT_HTML);
+        }
+    }
+}
+
+// Add bookmarks.
+$bookmarks = $VideoContent->interactiveVideo->assets->bookmarks;
+
+foreach ($bookmarks as $key => $value) {
+    $bookmarks[$key] = (array) $value;
+    $bookmarks[$key]['startSecond'] = $bookmarks[$key]['time'];
+}
+
+foreach ($c as $ckey => $line) {
+    foreach ($bookmarks as $key => $bookmark) {
+
+            if ($bookmark['time'] <= $line['startSecond']) {
+                $c[$ckey-5] = $bookmark;
+                unset($bookmarks[$key]);
+                continue;
+            }
+    }
+}
+
+// foreach ($bookmarks as $key => $bookmark) {
+//     foreach ($c as $ckey => $line) {
+//         $bt = $bookmark['time'];
+
+//             if ($bt <= $line['startSecond']) {
+//                 $c[$ckey-5] = $bookmark;
+//                 unset($bookmarks[$key]);
+//                 continue;
+//             }
+//     }
+// }
+var_dump($c);
+
+exit();
+
+echo '<p id="subtitle">'."\n";
+
+// foreach ($c as $key => $sub) {
+//     echo sprintf('<span data-index="%d" data-min="%d" data-max="%d">%s</span>',
+//         $sub['index'],
+//         $sub['startSecond'],
+//         $sub['endSecond'],
+//         $sub['content']
+//     );
+//     echo "\n";
+// }
+
 echo '</p>';
+/*
+echo '<span class="bookmark">First bookmark title</span><br>';
+echo '<span data-min="1" data-max="4">'.format_text('Lorem ipsum dolor sit amet, consectetur \( \sqrt[a]{b+c} \) adipiscing elit.', FORMAT_HTML).'</span>';
+echo '<span data-min="4" data-max="8">Morbi a justo vehicula, venenatis sapien ut, rhoncus turpis. Pellentesque sit amet dapibus orci. Sed varius nisl ipsum, non semper massa pellentesque et.</span>';
+echo '<span data-min="8" data-max="12">Sed hendrerit ut ante non fermentum.</span>';
+echo '<span data-min="12" data-max="16">Sed in turpis at augue mollis sodales.</span>';
+echo '<span data-min="16" data-max="19">Morbi neque libero, eleifend id tincidunt ut, dignissim at velit.</span>';
+echo '<span class="bookmark">Second bookmark title</span><br>';
+echo '<span data-min="19" data-max="21">Mauris tempus facilisis leo ac auctor. Integer a tempor magna.</span>';
+echo '<span data-min="21" data-max="24">Pellentesque sit amet dapibus orci. Sed varius nisl ipsum, non semper massa pellentesque et.</span>';
+echo '<span data-min="25" data-max="28">Sed tincidunt dapibus nisi vitae dignissim.</span>';
+echo '<span data-min="28" data-max="30">Maecenas vitae lacus finibus, maximus orci eget, accumsan urna.</span>';
+echo '<span data-min="30" data-max="32">Nulla condimentum eros in pellentesque laoreet.</span>';
+echo '<span data-min="32" data-max="34">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>';
+echo '<span class="bookmark">Third bookmark title</span><br>';
+echo '<span class="bookmark">Fourth bookmark title</span><br>';
+*/
 
 echo $OUTPUT->footer();
